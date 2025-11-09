8ebffc1e862f748676c46d101070d9702f4e51bf
Puxpui <pukpuidesu666@gmail.com>
2025-11-10 02:13:47 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
     <div className="w-full">
       <nav className="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur-md h-[65px] w-full text-black z-50 shadow-sm">
         <div className="flex justify-between items-center px-6 md:px-[45px] h-full">
-          <div className="text-[20px] font-bold cursor-pointer">Puxpui</div>
+          <div onClick={() => scrollToSection("hero-section")} className="text-[20px] select-none font-bold cursor-pointer">Puxpui</div>
           {isClicked && (
             <div className="hidden md:flex gap-[25px] items-center font-semibold text-[17px]">
               <button onClick={() => scrollToSection("hero-section")} className="nav-hover text-black hover:text-violet-600 transition">Home</button>
